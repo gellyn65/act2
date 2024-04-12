29c6883b581d07b1658bae35d3f787ba5769a3f4
angellyn <guardianangellyn65@gmail.com>
2024-04-12 19:58:12 +0800

    first commit

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>{{ product.name }}</h3>
+    <p>{{ product.description }}</p>
+    <p>Price: {{ product.price }}</p>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Product List</h2>
+    <div v-if="products.length > 0">
+      <ProductItem v-for="product in products" :key="product.id" :product="product" />
+    </div>
+    <div v-else>
+      <p>No products available.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue'; // Import the ProductItem component
+
+export default {
+  components: {
+    ProductItem
+  },
+  data() {
+    return {
+      products: [
+        { id: 1, name: 'Product A', description: 'Description A', price: '$10' },
+        { id: 2, name: 'Product B', description: 'Description B', price: '$20' },
+        { id: 3, name: 'Product C', description: 'Description C', price: '$30' }
+      ]
+    };
+  }
+}
+</script>
